6471a97abea3f2b91c454e9b7717f5c12e7257b3
Michael Ngo <michaeln56@gmail.com>
2020-02-28 12:59:51 -0500

    setupContract on initial usage of router
    Signed-off-by: Michael Ngo <michaeln56@gmail.com>

--- routes/responses.js
+++ routes/responses.js
@@ -8,33 +8,32 @@ let contract;
 let response = [];
 
 async function setupContract(){
-  const abi = await fetch('http://localhost:8888/api/DecentTakeHome/all?path=abi').then(res => res.json());
-  const address = await fetch('http://localhost:8888/api/DecentTakeHome/').then(res => res.json());
+  try{
+    const abi = await fetch('http://localhost:8888/api/DecentTakeHome/all?path=abi').then(res => res.json());
+    const address = await fetch('http://localhost:8888/api/DecentTakeHome/').then(res => res.json());
 
-  let provider = new Web3.providers.WebsocketProvider("ws://localhost:8545");
+    let provider = new Web3.providers.WebsocketProvider("ws://localhost:8545");
 
-  const web3 = new Web3(provider);
-  contract = new web3.eth.Contract(abi[0],address.address);
+    const web3 = new Web3(provider);
+    contract = new web3.eth.Contract(abi[0],address.address);
+    let greeting = await contract.events.NewString().on('data', function(result) {
+      if(!response.some(function(el){ return el.id === result.returnValues.id}))
+        response.push({
+          id: result.returnValues.id,
+          string: result.returnValues.content,
+          transformed: crypto
+              .createHash('md5')
+              .update(result.returnValues.id+result.returnValues.content, 'utf-8')
+              .digest('hex')});
+    });
+  } catch(err){
+    console.log('error in setting up contract: ', err);
+  }
 }
 
 /* GET responses listing. */
 router.get('/', async function(req, res, next) {
-  try{
-    await setupContract();
-
-    let greeting = await contract.events.NewString().on('data', function(result) {
-    if(!response.some(function(el){ return el.id === result.returnValues.id}))
-      response.push({
-        id: result.returnValues.id,
-        string: result.returnValues.content,
-        transformed: crypto
-            .createHash('md5')
-            .update(result.returnValues.id+result.returnValues.content, 'utf-8')
-            .digest('hex')});
-    });
-  } catch(err){
-    console.log('error in try catch: ', err);
-  }
   res.json(response);
 });
+setupContract();
 module.exports = router;
