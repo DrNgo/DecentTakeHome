31691cf4faab65858c4c360fe81a908c516b046a
Michael Ngo <michaeln56@gmail.com>
2020-03-04 13:36:52 -0500

    added try catch
    Signed-off-by: Michael Ngo <michaeln56@gmail.com>

--- app.js
+++ app.js
@@ -6,38 +6,43 @@ var logger = require('morgan');
 
 
 async function setupResponseRouter() {
-  var app = express();
+  try{
+    var app = express();
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
 
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    next(createError(404));
-  });
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      next(createError(404));
+    });
 
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
 
-  var responsesRouter = require('./routes/responses');
+    var responsesRouter = require('./routes/responses');
 
-  var responses = await responsesRouter();
-  app.use('/responses', responses);
-  return app;
+    var responses = await responsesRouter();
+    app.use('/responses', responses);
+    return app;
+  }catch(err){
+    console.log('error in app.js', err);
+  }
+
 }
 module.exports = setupResponseRouter;
