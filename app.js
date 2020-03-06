5e96b6feef7ad49de45abc2231dd22e45963dd3e
Michael Ngo <michaeln56@gmail.com>
2020-03-06 10:16:23 -0500

    expressjs only serves json
    Signed-off-by: Michael Ngo <michaeln56@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var responsesRouter = require('./routes/responses');
 
@@ -9,34 +7,25 @@ async function setupRouter() {
   try{
     var app = express();
 
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
-
     app.use(logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
 
     app.use('/responses', await responsesRouter());
 
     // catch 404 and forward to error handler
     app.use(function(req, res, next) {
-      next(createError(404));
+      next(createError(404))
     });
 
     // error handler
     app.use(function(err, req, res, next) {
-      // set locals, only providing error in development
-      res.locals.message = err.message;
-      res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-      // render the error page
       res.status(err.status || 500);
-      res.render('error');
+      res.json(err);
     });
+
     console.log('finish setting up app');
+
     return app;
   }catch(err){
     console.log('error in app.js', err);
